cdd683393391f1334c51654ba4086305533f1eb2
Vinícius Caetano Pereira <vinicius.caetanocp@gmail.com>
2023-09-30 20:09:59 -0300

    adicionando botão flutuante para abrir a janela com o link do formulário

--- javascript/main.js
+++ javascript/main.js
@@ -33,3 +33,15 @@ window.addEventListener('resize', function (event) {
         openSidebar();
     }
 });
+
+function abrirModal(){
+    const modal = document.getElementById('modal-container')
+    modal.classList.add('mostrar')
+
+    modal.addEventListener('click', (e) =>{
+        if (e.target.id == 'modal-container' || e.target.id == "fechar"){
+            modal.classList.remove('mostrar')
+            localStorage.fechaModal = 'modal-container'
+        }
+    })
+}
